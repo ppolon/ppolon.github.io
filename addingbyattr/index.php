4a9924fd73b572007f30667f2f2c2bd8ce14911c
Jonghyun <jhc@gist.ac.kr>
2023-05-27 16:28:39 +0900

    add back addingbyattr

---
+++ addingbyattr/index.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+<title>
+Jonghyun Choi @ ECE UMD
+</title>
+<LINK media = all href="../default2.css" type = text/css rel=StyleSheet>
+<!--STYLE type=text/css media=all>img{
+    PADDING-RIGHT: 0px; 
+	PADDING-LEFT: 0px; 
+	PADDING-BOTTOM: 0px; 
+	PADDING-TOP: 50px;
+	FLOAT: right;
+}
+</STYLE> 
+-->
+
+<body>
+
+<div id="container">
+
+<div class="research_title">
+Adding Unlabeled Samples to Categories by Learned Attributes
+</div>
+
+<div class="research_author">
+<a href="http://umiacs.umd.edu/~jhchoi">Jonghyun Choi</a>, <a href="http://umiacs.umd.edu/~mrastega">Mohammad Rastegari</a>, <a href="http://homes.cs.washington.edu/~ali/">Ali Farhadi</a> and <a href="http://umiacs.umd.edu/~lsd">Larry S. Davis</a><br>
+University of Maryland, College Park | University of Washington
+</div>
+<div class="research_conf">
+IEEE CVPR 2013
+</div>
+
+<div class="research_subtitle">
+</div>
+
+<p></p>
+
+<img src="./adding.jpg" width=920>
+
+<p> 
+<b> Abstract </b><br>
+We propose a method to expand the visual coverage of training sets that consist of a small number of labeled examples using learned attributes. Our optimization formulation discovers category specific attributes as well as the images that have high con.dence in terms of the attributes. In addition, we propose a method to stably capture example-specific attributes for a small sized training set. Our method adds images to a category from a large unlabeled image pool, and leads to signi.cant improvement in category recognition accuracy evaluated on a large-scale dataset, ImageNet.
+</p>
+
+<h2> Papers and Poster </h2>
+<p>
+<a href="../paper/cvpr2013_adding.pdf">[Paper]</a>
+<a href="./poster.pdf">[Poster]</a>
+<a href="../paper/cvprw2013_expanding.pdf">[Extended Abstract for SUNw Workshop]</a>
+</p>
+
+
+<h2> Download - Code and Dataset</h2> 
+<p>
+<a href="./adding_by_attr_data.rar">Dataset: 337 MB</a><br>
+<a href="https://github.com/ppolon/adding_by_attr/" target="_blank">Code [Matlab]: Github Page</a><br><br>
+Distribution license: MIT License
+</p>
+
+<h2> Citation </h2>
+<h6>
+<pre class="bibtex">
+@inproceedings{choiRFD13,
+  author    = {Jonghyun Choi and Mohammad Rastegari and Ali Farhadi and Larry S. Davis},
+  title     = {Adding Unlabeled Samples to Categories by Learned Attributes},
+  booktitle = {IEEE Conf. Computer Vision and Pattern Recognition (CVPR)},
+  year      = {2013}
+}
+</pre>
+</h6>
+
+<h2> Contact </h2>
+<p>
+Bug report or any questions<br>
+Send an email to Jonghyun Choi <a href="mailto:jhchoi_[at]_umd_[dot]_edu">jhchoi_[at]_umd_[dot]_edu</a>
+</p>
+
+
+<?php include("footer2.php"); ?>
+
+</body>
+
+</html>
